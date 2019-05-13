c2b1b7605bed42820480bd1e7596733691bbf561
vladabraha <vladislav.braha@gmail.com>
2019-05-13 21:45:23 +0200

    Implementation of map

---
+++ app/src/main/java/com/example/graphstheory/Circle.java
@@ -0,0 +1,17 @@
+package com.example.graphstheory;
+
+public class Circle {
+    private Coordinate coordinate;
+
+    public Circle(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+}
---
+++ app/src/main/java/com/example/graphstheory/Map.java
@@ -0,0 +1,30 @@
+package com.example.graphstheory;
+
+import java.util.ArrayList;
+
+public class Map {
+    private ArrayList<Line> lines;
+    private ArrayList<Circle> circles;
+
+
+    public Map(ArrayList<Line> lines, ArrayList<Circle> circles) {
+        this.lines = lines;
+        this.circles = circles;
+    }
+
+    public ArrayList<Line> getLines() {
+        return lines;
+    }
+
+    public void setLines(ArrayList<Line> lines) {
+        this.lines = lines;
+    }
+
+    public ArrayList<Circle> getCircles() {
+        return circles;
+    }
+
+    public void setCircles(ArrayList<Circle> circles) {
+        this.circles = circles;
+    }
+}
