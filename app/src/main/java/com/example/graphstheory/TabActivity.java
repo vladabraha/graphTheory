014beeb296591d573fbe9d754aee3937e42c415c
vladabraha <36154909+vladabraha@users.noreply.github.com>
2019-05-28 13:47:46 +0200

    Revert "Add tabLayout to Activity"
    This reverts commit 20c297db12f822ed71dd8868c107e530fc2ab433.

--- app/src/main/java/com/example/graphstheory/TabActivity.java
+++ app/src/main/java/com/example/graphstheory/TabActivity.java
@@ -8,10 +8,8 @@ import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TableLayout;
-import android.widget.Toast;
 
-public class TabActivity extends AppCompatActivity implements TableLayoutFragment.TableLayoutCommunicationInterface {
+public class TabActivity extends AppCompatActivity {
 
     private TextFragment fragment;
 
@@ -24,8 +22,6 @@ public class TabActivity extends AppCompatActivity implements TableLayoutFragmen
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         fragment = new TextFragment();
-        TableLayoutFragment tableLayoutFragment = new TableLayoutFragment();
-        fragmentTransaction.add(R.id.activity_group, tableLayoutFragment);
         fragmentTransaction.add(R.id.activity_group, fragment);
         fragmentTransaction.commit();
 
@@ -43,52 +39,21 @@ public class TabActivity extends AppCompatActivity implements TableLayoutFragmen
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.circle:
+//                paintView.circle();
                 fragment.changeDrawingMethod("circle");
                 return true;
             case R.id.line:
                 fragment.changeDrawingMethod("line");
+//                paintView.line();
                 return true;
+
             case R.id.clear:
                 fragment.changeDrawingMethod("clear");
+//                paintView.clear();
                 return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void tableLayoutSelectedChange(int number) {
-        switch (number){
-            case 0:
-                removeDrawingFragment();
-                break;
-            case 1:
-                removeDrawingFragment();
-                break;
-            case 2:
-                addDrawingFragment();
-                break;
-        }
-    }
-
-    private void removeDrawingFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        TableLayoutFragment tableLayoutFragment = new TableLayoutFragment();
-
-        fragmentTransaction.remove(fragment);
-        fragmentTransaction.commit();
-    }
-
-    private void addDrawingFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragment = new TextFragment();
-        TableLayoutFragment tableLayoutFragment = new TableLayoutFragment();
-
-        fragmentTransaction.add(R.id.activity_group, fragment);
-        fragmentTransaction.commit();
     }
 
     public interface OnFragmentInteractionListener {
